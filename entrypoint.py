6bd2804793f2d7372fd01bce954225bd43b5eb67
jose <jaltamonte@gmail.com>
2025-04-21 20:45:42 -0300

    config

--- entrypoint.py
+++ entrypoint.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-project_folder = os.path.expanduser('~/groma')  # adjust as appropriate
+project_folder = os.path.expanduser('~/ordeplos')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 
 
